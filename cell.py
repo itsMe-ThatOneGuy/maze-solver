5e7a7d2ab57ad055ed7fd5eebbcffc3312b3aeb8
Matthew Smart <matthewsmart13@gmail.com>
2024-10-18 23:03:35 -0500

    Added a white line to draw

--- cell.py
+++ cell.py
@@ -24,15 +24,28 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self.win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self.win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.win.draw_line(line, "white")
+
 
     def draw_move(self, to_cell, undo=False):
         half = abs(self.x2 - self.x1) // 2
